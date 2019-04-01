5e313467d7dc9804c8136a203b55aac4e1b8d1a1
= <devleonardo@hotmail.com>
2019-04-01 12:32:50 -0300

    Adicionando eventos de mouse

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function clicarButton(){
+    alert("CLICOUUUUUUUUUUUUUUUUUUUUUUUUUUUUU !");
+}
+
+function clicarDuasButton(){
+    alert("DUBLE CLICK !!!");
+}
+
+function rightButton(){
+    alert("Você não pode apertar como botão direito no meu sistema !");
+    return false;
+}
+
+function apertarButton(){
+    console.log("Você esta apertando o meu botão!");
+}
+
+function soltarButton(){
+    console.log("Você soltou o botão!");
+}
+
+function passarButton(){
+    console.log("Você esta passando por cima do meu botão");
+}
+
+function sairButton(){
+    console.log("Boa saiu de cima");
+}
+
+function moveButton(){
+    console.log("Tu continua passando por cima de min!")
+}
